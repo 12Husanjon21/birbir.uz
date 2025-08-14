cccce39d396f6d66aaab4db35dc4efc9b9aeabab
Husanjon <ismoilovhusanjon01@gmail.com>
2025-08-14 15:24:03 +0500

    HomePage changes

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Button() {
+  return (
+    <div className="mx-auto max-w-[170px] mb-[130px]">
+      <button className="bg-[#27383F] text-white cursor-pointer py-4 px-5 rounded-[18px] hover:bg-[#4c5e7c] transition-colors duration-300">
+        Yana ko'rsatish
+      </button>
+    </div>
+  );
+}
