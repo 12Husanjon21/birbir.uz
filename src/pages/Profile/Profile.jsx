d61f1d0f56fda3b86aa6aec26b42ad3ab9dddbe5
AzizbekGayratov <azizbekgairatov@gmail.com>
2025-09-30 23:44:12 +0500

    auth delete

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import LogOut from "./LogOut";
+// import LogOut from "./LogOut";
 
 export default function Profile() {
   const profileData = useSelector((state) => state.project);
@@ -10,7 +10,7 @@ export default function Profile() {
     <div className="page-module__page--lr4UP">
       <div className="container-module__container--dFCmu favoriteContainer-module__favorite--A_e2h">
         <h1>{profileData?.user?.telegram_username}</h1>
-        <LogOut userId={profileData?.user?.id}/>
+        {/* <LogOut userId={profileData?.user?.id}/> */}
       </div>
     </div>
   );
